b7b98c4bc47eaa3e07d7701d9eb456128d077254
Gary McDaniel <gary_mcdaniel@comcast.net>
2018-07-08 23:16:13 -0600

    created the base and blog templates, modified database in main

--- main.py
+++ main.py
@@ -8,13 +8,44 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = "#someSecretString"
 
+#setup a Blog class
+
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.column(db.String(1000))
 
-    def __init__(self, name):
+    def __init__(self, title, body):
         self.title = title
         self.body = body
         
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        task_name = request.form['task']
+        new_task = Task(task_name)
+        db.session.add(new_task)
+        db.session.commit()
+
+    tasks = Task.query.filter_by(completed=False).all()
+    completed_tasks = Task.query.filter_by(completed=True).all()
+    return render_template('todos.html',title="Get It Done!", 
+        tasks=tasks, completed_tasks=completed_tasks)
+
+
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
+
+    task_id = int(request.form['task-id'])
+    task = Task.query.get(task_id)
+    task.completed = True
+    db.session.add(task)
+    db.session.commit()
+
+    return redirect('/blog')
+
+
+if __name__ == '__main__':
+    app.run()
